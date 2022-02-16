aa7e880193459c5b4909e9d6c9da268b2e9efa85
Michael Tuma <mike.tuma@hotmail.com>
2022-02-15 20:03:59 -0500

    This is why we test code before pushing...

--- cstermproj/__init__.py
+++ cstermproj/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 flaskapp = Flask(__name__)
 
-@app.route("/")
+@flaskapp.route("/")
 def index():
 	return "<html><head><title>howdy</title></head><body>extremely simple test</body></html>"
