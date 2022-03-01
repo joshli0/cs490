bbd0e5b33446ccc982d190afa340d831ba213a69
Michael Tuma <mike.tuma@hotmail.com>
2022-02-28 21:16:49 -0500

    Fixed crashes with database connection system when not using select queries and added support for selecting results as a dictionary

--- cstermproj/backend/dbconn.py
+++ cstermproj/backend/dbconn.py
@@ -1,5 +1,7 @@
 import os
+
 import psycopg2
+import psycopg2.extras
 
 con = None
 
@@ -7,11 +9,25 @@ def connect():
 	global con
 	con = psycopg2.connect(os.environ["DATABASE_URL"], sslmode = "require")
 
-def query(query, *args):
-	cur = con.cursor()
+def query(query, *args, as_dict = False):
+	cur_params = {}
 	
+	if as_dict:
+		cur_params["cursor_factory"] = psycopg2.extras.RealDictCursor
+	
+	cur = con.cursor(**cur_params)
 	cur.execute(query, *args)
-	res = cur.fetchall()
+	
+	try:
+		res = cur.fetchall()
+	except psycopg2.ProgrammingError:
+		res = None
+	
+	if as_dict and res is not None:
+		res = [dict(r) for r in res]
 	
 	cur.close()
 	return res
+
+def commit():
+	con.commit()
