4780212a8989bb44030d006a308763c319fe05af
Michael Tuma <mike.tuma@hotmail.com>
2022-03-04 16:01:54 -0500

    Teachers are now prevented from seeing student pages, and vis-versa

--- cstermproj/frontend/__init__.py
+++ cstermproj/frontend/__init__.py
@@ -8,33 +8,33 @@ def startup(flaskapp):
 
 	@flaskapp.route("/app", methods = ["GET"])
 	def app():
-
 		args = flask.request.args
-
+		
 		if "page" in args:
 			page = args["page"]
 		else:
 			page = "landing"
-
+		
 		if page == "landing":
 			return landing()
-		elif page == "manage_questions":
-			return manage_questions()
-		elif page == "manage_exams":
-			return manage_exams()
-		elif page == "grade_exams":
-			return grade_exams()
-		elif page == "review_exam":
-			return review_exam()
-		elif page == "exam_list":
-			return exam_list()
-		elif page == "view_results":
-			return view_results()
-		elif page == "take_exam":
-			return take_exam()
-		elif page == "results":
-			return results()
+		
+		if flask.session["teacher"]:
+			if page == "manage_questions":
+				return manage_questions()
+			elif page == "manage_exams":
+				return manage_exams()
+			elif page == "grade_exams":
+				return grade_exams()
+			elif page == "review_exam":
+				return review_exam()
 		else:
-			return flask.redirect("/app")
-
-		return landing()
+			if page == "exam_list":
+				return exam_list()
+			elif page == "view_results":
+				return view_results()
+			elif page == "take_exam":
+				return take_exam()
+			elif page == "results":
+				return results()
+		
+		return flask.redirect("/app")
