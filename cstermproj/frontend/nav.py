3c29c8e8dba9d0186c914958add5b8e452a64c7a
KGB6\Kayli Brower <kgb6@njit.edu>
2022-03-04 16:09:46 -0500

    Calling functions from backend

--- cstermproj/frontend/nav.py
+++ cstermproj/frontend/nav.py
@@ -1,12 +1,21 @@
 import flask
 from .landing import landing
+from ..backend.questions import *
 
 # Teacher pages
 def manage_questions():
-    return flask.render_template("manage_questions.html")
+    return flask.render_template("manage_questions.html",
+    categories=get_categories(),
+    difficulties=get_difficulties(),
+    questionBank=get_all_questions(),
+    questionid=get_question_ids())
 
 def manage_exams():
-    return flask.render_template("manage_exams.html")
+    return flask.render_template("manage_exams.html",
+    categories=get_categories(),
+    difficulties=get_difficulties(),
+    questionBank=get_all_questions(),
+    id=get_question_ids())
 
 def grade_exams():
     return flask.render_template("grade_exams.html")
