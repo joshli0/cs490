4b4e45647d49a2812ee4c64d7ac0878343f59e92
KGB6\Kayli Brower <kgb6@njit.edu>
2022-03-06 16:59:01 -0500

    Fixed for manage/build exam pages

--- cstermproj/middleend/testSend.py
+++ cstermproj/middleend/testSend.py
@@ -6,22 +6,24 @@ def send(flaskapp):
     @flaskapp.route("/create_test", methods = ["POST", "GET"])
     def createTest():
         data = request.values
-        if 'name' in data:
-            name = data['name']
+        redir = "manage_exams"
+        if 'examname' in data:
+            name = data['examname']
             create_test(name)
-        
-        return redirect('/app?page=manage_exams')
+            redir = "build_exam&title=" + name
+
+        return redirect('/app?page=' + redir)
 
     @flaskapp.route("/add_question", methods = ["POST", "GET"])
     def addQuestion():
         data = request.values
         if 'test_id' in data and 'question_id' in data and \
             'point_value' in data:
-            test_id = data['test_id'] 
-            question_id = data['question_id'] 
-            point_value = data['point_value']
+            test_id = int(data['test_id'])
+            question_id = int(data['question_id'])
+            point_value = int(data['point_value'])
             add_question(test_id, question_id, point_value)
-        
+
         return redirect('/app?page=manage_exams')
 
     @flaskapp.route("/remove_question", methods = ["POST", "GET", "DELETE"])
@@ -29,10 +31,10 @@ def send(flaskapp):
         data = request.values
         if 'test_id' in data and 'question_id' in data and \
             'point_value' in data:
-            test_id = data['test_id'] 
-            question_id = data['question_id'] 
+            test_id = data['test_id']
+            question_id = data['question_id']
             remove_question(test_id, question_id)
-        
+
         return redirect('/app?page=manage_exams')
 
     @flaskapp.route("/delete_test", methods = ["POST", "GET", "DELETE"])
@@ -41,7 +43,7 @@ def send(flaskapp):
         if 'name' in data:
             name = data['name']
             delete_test(name)
-        
+
         return redirect('/app?page=manage_exams')
 
     @flaskapp.route("/set_test_comments", methods = ["POST", "GET"])
@@ -49,12 +51,12 @@ def send(flaskapp):
         data = request.values
         if 'name_or_id'in data and 'student_name'in data and \
             'comments_on_questions' in data and 'comments_on_whole_test' in data:
-            name_or_id = data['name_or_id'] 
-            student_name = data['student_name'] 
-            comments_on_questions = data['comments_on_questions'] 
+            name_or_id = data['name_or_id']
+            student_name = data['student_name']
+            comments_on_questions = data['comments_on_questions']
             comments_on_whole_test = data['comments_on_whole_test']
             set_test_comments(name_or_id, student_name, comments_on_questions, comments_on_whole_test)
-        
+
         return redirect('/app?page=grade_exams')
 
     @flaskapp.route("/set_test_manual_grades", methods = ["POST", "GET"])
@@ -62,11 +64,11 @@ def send(flaskapp):
         data = request.values
         if 'name_or_id'in data and 'student_name_or_id' in data and \
             'manual_grades' in data:
-            name_or_id = data['name_or_id'] 
-            student_name_or_id = data['student_name'] 
+            name_or_id = data['name_or_id']
+            student_name_or_id = data['student_name']
             manual_grades = data['manual_grades']
             set_test_manual_grades(name_or_id, student_name_or_id, manual_grades)
-        
+
         return redirect('/app?page=grade_exams')
 
     @flaskapp.route("/set_test_auto_grades", methods = ["POST", "GET"])
@@ -74,11 +76,11 @@ def send(flaskapp):
         data = request.values
         if 'name_or_id'in data and 'student_name_or_id' in data and \
             'auto_grades' in data:
-            name_or_id = data['name_or_id'] 
-            student_name_or_id = data['student_name'] 
+            name_or_id = data['name_or_id']
+            student_name_or_id = data['student_name']
             auto_grades = data['auto_grades']
             set_test_auto_grades(name_or_id, student_name_or_id, auto_grades)
-        
+
         return redirect('/app?page=grade_exams')
 
     @flaskapp.route("/set_test_grades_released", methods = ["POST", "GET"])
@@ -86,11 +88,11 @@ def send(flaskapp):
         data = request.values
         if 'name_or_id'in data and 'student_name_or_id' in data and \
             'released' in data:
-            name_or_id = data['name_or_id'] 
-            student_name_or_id = data['student_name'] 
+            name_or_id = data['name_or_id']
+            student_name_or_id = data['student_name']
             released = data['released']
             set_test_auto_grades(name_or_id, student_name_or_id, released)
-        
+
         return redirect('/app?page=grade_exams')
 
     @flaskapp.route("/submit_test_response", methods = ["POST", "GET"])
@@ -98,11 +100,11 @@ def send(flaskapp):
         data = request.values
         if 'name_or_id'in data and 'student_name_or_id' in data and \
             'responses' in data:
-            name_or_id = data['name_or_id'] 
-            student_name_or_id = data['student_name'] 
+            name_or_id = data['name_or_id']
+            student_name_or_id = data['student_name']
             responses = data['responses']
             submit_test_response(name_or_id, student_name_or_id, responses)
-        
+
         return redirect('/app?page=exam_list')
 
     @flaskapp.route("/set_test_case_outputs", methods = ["POST", "GET"])
@@ -110,20 +112,20 @@ def send(flaskapp):
         data = request.values
         if 'name_or_id'in data and 'student_name_or_id' in data and \
             'test_case_outputs' in data:
-            name_or_id = data['name_or_id'] 
-            student_name_or_id = data['student_name'] 
+            name_or_id = data['name_or_id']
+            student_name_or_id = data['student_name']
             test_case_outputs = data['test_case_outputs']
             set_test_case_outputs(name_or_id, student_name_or_id, test_case_outputs)
-        
+
         return redirect('/app?page=take_exam')
-    
+
 #get test id
 def getTestID(name):
     return get_test_id(name)
 
 #does test exist
 def doesTestExist(name):
-    return does_test_exist(name)   
+    return does_test_exist(name)
 
 #does test contain question
 def doesTestContainQuestion(test_id, question_id):
@@ -168,7 +170,7 @@ def getTestIDS():
 #get test names
 def getTestNames():
     return get_test_names()
-    
+
 #get_all_responses
 def getAllResponses():
     return get_all_responses()
