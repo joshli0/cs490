66d704f5255c2d0d17f4151e5534ac658bd873d4
Michael Tuma <mike.tuma@hotmail.com>
2022-03-06 22:03:36 -0500

    Added accessor for username

--- cstermproj/backend/login.py
+++ cstermproj/backend/login.py
@@ -6,7 +6,13 @@ def sha256(text):
 	return hashlib.sha256(text.encode("utf8")).hexdigest()
 
 def check_credentials(username, password):
-	results = query("select AccessType from Users where Username = %s and PassHash = %s", (username, sha256(password)))
+	results = query("select AccessType from CS490Proj.Users where Username = %s and PassHash = %s", (username, sha256(password)))
 	
 	if results is not None and len(results) == 1:
 		return results[0][0] == "Teacher"
+
+def get_username(id):
+	results = query("select Username from CS490Proj.Users where ID = %s", (id, ))
+	
+	if results is not None and len(results) == 1:
+		return results[0][0]
